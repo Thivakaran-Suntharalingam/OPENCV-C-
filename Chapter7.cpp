81e361fc99b36eb20b06d6339bf020227b366654
Thivakaran Suntharalingam <73923739+thivakarann@users.noreply.github.com>
2021-12-09 17:04:11 +0530

    Update Chapter7.cpp

--- Chapter7.cpp
+++ Chapter7.cpp
@@ -6,34 +6,69 @@
 using namespace cv;
 using namespace std;
 
-/////////////// Color Detection //////////////////////
+/////////////// Shape or Contour Detection //////////////////////
 
-Mat imgHSV, mask;
-int hmin = 0, smin = 0, vmin = 0;
-int hmax = 179, smax = 255, vmax = 255;
+void getContours(Mat imgDil, Mat img) {
+
+	vector<vector<Point>> contours;
+	vector<Vec4i> hierarchy;
+
+	findContours(imgDil, contours, hierarchy, RETR_EXTERNAL, CHAIN_APPROX_SIMPLE);
+	//drawContours(img, contours, -1, Scalar(255, 0, 255), 2);
+
+	vector<vector<Point>> conPoly(contours.size());
+	vector<Rect> boundRect(contours.size());
+	for (int i = 0; i < contours.size(); i++)
+	{
+		int area = contourArea(contours[i]);
+		cout << area << endl;
+		string objectType;
+		if (area > 1000)
+		{
+			float peri = arcLength(contours[i], true);
+			approxPolyDP(contours[i], conPoly[i], 0.02 * peri, true);
+			cout << conPoly[i].size() << endl;
+			boundRect[i] = boundingRect(conPoly[i]);
+
+			int objCor = (int)conPoly[i].size();
+
+			if (objCor == 3) { objectType = "Tri"; }
+			else if (objCor == 4)
+			{
+				float aspRatio = (float)boundRect[i].width / (float)boundRect[i].height;
+				cout << aspRatio << endl;
+				if (aspRatio > 0.95 && aspRatio < 1.05) { objectType = "Square"; }
+				else { objectType = "Rect"; }
+			}
+			else if (objCor > 4) { objectType = "Circle"; }
+
+			drawContours(img, conPoly, i, Scalar(255, 0, 255), 2);
+			rectangle(img, boundRect[i].tl(), boundRect[i].br(), Scalar(0, 255, 0), 5);
+			putText(img, objectType, { boundRect[i].x,boundRect[i].y-5 }, FONT_HERSHEY_PLAIN, 1, Scalar(0, 69, 255), 2);
+		}
+	}
+}
 void main() {
 
-	// Blank image
-	string path = "Resources/lambo.png";
+	string path = "Resources/shapes.png";
 	Mat img = imread(path);
+	Mat imgGray, imgBlur, imgCanny, imgDil, imgErode;
 
-	cvtColor(img, imgHSV, COLOR_BGR2HSV);
-	namedWindow("Trackbars", (640, 200));
-	createTrackbar("Hue min", "Trackbars", &hmin, 179);
-	createTrackbar("Hue max", "Trackbars", &hmax, 179);
-	createTrackbar("Sat min", "Trackbars", &smin, 255);
-	createTrackbar("Sat max", "Trackbars", &smax, 255);
-	createTrackbar("Val min", "Trackbars", &vmin, 255);
-	createTrackbar("Val max", "Trackbars", &vmax, 255);
+	// Preprocessing
+	cvtColor(img, imgGray, COLOR_BGR2GRAY);
+	GaussianBlur(imgGray, imgBlur, Size(3, 3), 3, 0);
+	Canny(imgBlur, imgCanny, 25, 75);
+	Mat kernel = getStructuringElement(MORPH_RECT, Size(3, 3));
+	dilate(imgCanny, imgDil, kernel);
 
-	while (true) {
-		Scalar lower(hmin, smin, vmin);
-		Scalar upper(hmax, smax, vmax);
-		inRange(imgHSV, lower, upper, mask);
+	getContours(imgDil, img);
 
-		imshow("Image", img);
-		imshow("Image HSV", imgHSV);
-		imshow("Image Mask", mask);
-		waitKey(1);
-	}
+	imshow("Image", img);
+	//imshow("Image Gray", imgGray);
+	//imshow("Image Blur", imgBlur);
+	//imshow("Image Canny", imgCanny);
+	//imshow("Image Dil", imgDil);
+
+	waitKey(0);
+
 }
